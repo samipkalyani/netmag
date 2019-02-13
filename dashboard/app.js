530c3676a8437f20308a29d0dfd1d613872014d6
shahvimal <shahvimal5009@gmail.com>
2019-02-13 08:05:41 +0530

    second

---
+++ dashboard/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path =require("path");
+
+app = express();
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.render("dashboard.ejs");
+});
+
+app.get("/action1",(req,res)=>{
+    res.render("action1.ejs");
+});
+
+app.listen(3000,()=>{
+    console.log("server started");
+});
