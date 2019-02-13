54283ed229059947c4c6271fc4da518d9f7df5ed
vidhi <samipkalyani004@gmail.com>
2019-02-13 11:16:56 +0530

    third

--- dashboard/app.js
+++ dashboard/app.js
@@ -13,6 +13,14 @@ app.get("/action1",(req,res)=>{
     res.render("action1.ejs");
 });
 
+app.get("/dashboard",(req,res)=>{
+    res.render("dashboard.ejs");
+});
+
+app.get("/addpatient",(req,res)=>{
+    res.render("addpatient.ejs");
+});
+
 app.listen(3000,()=>{
     console.log("server started");
 });
